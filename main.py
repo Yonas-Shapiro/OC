dc3d09937d53f399f0d3149db4142fb82c95e22c
Yonas <yogiorno05@gmail.com>
2022-08-29 16:11:21 -0400

    Able to log in

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 # Main file for selenium
 
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+#driver.close()
+vmNum = input("VM num: ")
+site = "http://192.168.2." + str(vmNum) + "/otcs/cs.exe?func=llworkspace"
+
+driver.get(site)
+print(driver.title)
+
+driver.find_element(By.ID, "otds_username").send_keys("Admin")
+driver.find_element(By.ID, "otds_password").send_keys("livelink")
+driver.find_element(By.ID, "otds_password").send_keys(Keys.RETURN)
