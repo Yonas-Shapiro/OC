651f39e7cb1acd2daa28a7b17a47db8c7ba2ce39
Yonas <yogiorno05@gmail.com>
2022-08-29 21:22:29 -0400

    Language change possible now!

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+from languages import switchLanguage
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -18,3 +21,9 @@ print(driver.title)
 driver.find_element(By.ID, "otds_username").send_keys("Admin")
 driver.find_element(By.ID, "otds_password").send_keys("livelink")
 driver.find_element(By.ID, "otds_password").send_keys(Keys.RETURN)
+
+print(driver.title)
+
+
+
+switchLanguage(driver, "de", site)
